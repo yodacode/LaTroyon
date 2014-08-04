67e4680e7f9d62bbccc6ebe6b5113e964512cb85
yodacode <benjamin.devaublanc@gmail.com>
2014-08-04 15:54:59 +0200

    update mpc

--- js/app.js
+++ js/app.js
@@ -72,7 +72,11 @@ $(function () {
     App.Mpc = {
         init: function () {
             this.UI = {};
+            this.status = {
+                isDisplay: false
+            };
             this.UI.mpc = $('.mpc');
+            this.UI.btnMpc = $('.mpc-btn');
 
             this.UI.keyboard = null;
 
@@ -80,8 +84,11 @@ $(function () {
             this.bind();
         },
 
+
         build: function () {
-            var i;
+            var i, mask,
+                that = this,
+                height = $('body').height();
 
             this.UI.keyboard = $('<div>')
                 .addClass('mpc-keyboard')
@@ -90,6 +97,74 @@ $(function () {
             for (i in assoc) {
                 this.createKey(i, assoc[i].letter, assoc[i].soundId);
             };
+
+
+            this.UI.mask = $('<div>')
+                .addClass('mpc-mask')
+                .css({height: height})
+                .appendTo($('body'))
+                .on('click', function () {
+                    that.display(false);
+                });
+
+
+        },
+
+        bind: function () {
+
+            var that = this;
+
+            this.UI.mpc.on('click', '.mpc-keyboard-key', function () {
+                var playerAudio  = $(this).find('audio')[0];
+                that.play(playerAudio);
+            });
+
+            this.UI.btnMpc.on('click', function () {
+                if (that.status.isDisplay) {
+                    that.display(false);
+                } else {
+                    that.display(true);
+                }
+            });
+        },
+
+        display: function (isDisplay) {
+            var that = this;
+
+                this.UI.mpc.stop().animate({
+                    top: isDisplay ? '80px' : '-800px'
+                }, 500, function () {
+                    that.bindKeyboard(isDisplay);
+                    that.status.isDisplay = isDisplay;
+                    
+                    if (isDisplay) {                    
+                        that.UI.mask.fadeIn();
+                    } else {
+                        that.UI.mask.fadeOut();
+                    }
+                });
+            
+        },
+
+        bindKeyboard: function (isBind) {
+
+            var doc = $(document);
+
+            if (isBind) {
+                doc.on('keydown', function(e) {
+                    var key  = $('.mpc-keyboard-key[data-code=' + e.keyCode + ']');
+                        key.trigger('click');
+                        key.addClass('active');
+                });
+
+                doc.on('keyup', function(e) {
+                    var key  = $('.mpc-keyboard-key[data-code=' + e.keyCode + ']');
+                        key.removeClass('active');
+                });
+            } else {
+                doc.unbind('keydown');
+                doc.unbind('keyup');
+            }
         },
 
         createKey: function (code, letter, soundId) {
@@ -126,30 +201,9 @@ $(function () {
 
         },
 
-        bind: function () {
 
-            var that = this;
-
-            this.UI.mpc.on('click', '.mpc-keyboard-key', function () {
-                var playerAudio  = $(this).find('audio')[0];
-                that.play(playerAudio);
-            });
-
-            $(document).on('keydown', function(e) {
-                var key  = $('.mpc-keyboard-key[data-code=' + e.keyCode + ']');
-                    key.trigger('click');
-                    key.addClass('active');
-            });
-
-            $(document).on('keyup', function(e) {
-                var key  = $('.mpc-keyboard-key[data-code=' + e.keyCode + ']');
-                    key.removeClass('active');
-            });
-        },
 
     }
-
-    App.Mpc.init();
 
 	App.Sound = {
 		init: function () {
@@ -163,6 +217,7 @@ $(function () {
 
 			this.getSounds(function (sounds) {
 				that.build(sounds);
+                App.Mpc.init();
 			});
 		},
 
